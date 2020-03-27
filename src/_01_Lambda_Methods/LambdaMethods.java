6cba02b084ce421ec5421e1fbe83ae05fafc4725
Louis <louisderrien28@gmail.com>
2020-03-26 18:32:30 -0700

    on stream search number 4

--- src/_01_Lambda_Methods/LambdaMethods.java
+++ src/_01_Lambda_Methods/LambdaMethods.java
@@ -1,5 +1,8 @@
 package _01_Lambda_Methods;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class LambdaMethods {
 	public static void main(String[] args) {
 		// 1. Look at the SpecialPrinter function interface.
@@ -13,13 +16,50 @@ public class LambdaMethods {
 		}, "repeat");
 		
 		//2. Call the printCustonMessage method using a lambda so that the String prints backwards.
+		printCustomMessage((String s)->{
 		
+			String ans = "";
+			for(int i = s.length()-1; i>=0;i--) {
+				ans += s.charAt(i);
+			}
+		}, "repeat");
 		//3. Call the printCustonMessage method using a lambda so that the String prints with a mix between upper an lower case characters.
-		
+		printCustomMessage((String s)->{
+			String ans  = "";
+			for(int i = 0; i<s.length();i++) {
+				int r = new Random().nextInt(2);
+				if(r == 0) {
+					char c = s.charAt(i);
+					Character.toUpperCase(c);
+					ans +=c;
+				}
+				else {
+					char c = s.charAt(i);
+					Character.toLowerCase(c);
+					ans +=c;
+				}
+			}
+		}, "repeat");
 		//4. Call the printCustonMessage method using a lambda so that the String prints with a period in between each character.
-		
+		printCustomMessage((String s)->{
+			String ans = "";
+			for(int i = 0; i<s.length();i++) {
+				ans += s.charAt(i);
+				ans += ".";
+			}
+		}, "repeat");
 		//5. Call the printCustonMessage method using a lambda so that the String prints without any vowels.
-	
+		printCustomMessage((String s)->{
+			String ans ="";
+			for(int i = 0; i<s.length(); i++) {
+				if(s.charAt(i) == 'a' || s.charAt(i) == 'e'|| s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u' || s.charAt(i) == 'y') {
+					
+				}
+				else {
+					ans += s.charAt(i);
+				}
+			}
+		}, "repeat");
 	}
 	
 	public static void printCustomMessage(SpecialPrinter sp, String value) {
